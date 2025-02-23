0487e2681679921b5c7894fdae8036e5be20d217
chamasei <yuki.saxophonist@gmail.com>
2025-02-23 18:22:47 +0900

    Fix PORT binding issue

--- app.py
+++ app.py
@@ -565,3 +565,5 @@ def internal_server_error(e):
 
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render の `$PORT` を取得
+    app.run(host="0.0.0.0", port=port, debug=False)  # `debug=False` にする
